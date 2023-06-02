f256447a2fb59068a9972fc56e3af6fc7a3ef93c
ZAESKY <zaesky123@163.com>
2023-06-02 15:51:47 +0800

    增加关注用户功能

--- src/api/user.js
+++ src/api/user.js
@@ -35,3 +35,22 @@ export const getUserChannels = () => {
     url: '/v1_0/user/channels'
   })
 }
+
+// 关注用户
+export const addUserFollow = target => {
+  return request({
+    method: 'POST',
+    url: '/v1_0/user/followings',
+    data: {
+      target
+    }
+  })
+}
+
+// 取消关注用户
+export const deleteUserFollow = target => {
+  return request({
+    method: 'DELETE',
+    url: `/v1_0/user/followings/${target}`
+  })
+}
--- src/views/article/ArticleDetail.vue
+++ src/views/article/ArticleDetail.vue
@@ -26,7 +26,7 @@
         <!-- /文章标题 -->
 
         <!-- 用户信息 -->
-        <van-cell class="user-info" center :border="false">
+        <van-cell class="art_user-info" center :border="false">
           <van-image
             class="avatar"
             slot="icon"
@@ -36,19 +36,26 @@
           />
           <div slot="title" class="user-name">{{article.aut_name}}</div>
           <div slot="label" class="publish-date">{{article.pubdate | relativeTime}}</div>
+          <!-- 关注按钮 -->
           <van-button
+            v-if="article.is_followed"
+            class="follow-btn"
+            round
+            size="small"
+            :loading="followLoading"
+            @click="onFollow"
+          >已关注</van-button>
+          <van-button
+            v-else
             class="follow-btn"
             type="info"
             color="#3296fa"
             round
             size="small"
             icon="plus"
+            :loading="followLoading"
+            @click="onFollow"
           >关注</van-button>
-          <!-- <van-button
-            class="follow-btn"
-            round
-            size="small"
-          >已关注</van-button> -->
         </van-cell>
         <!-- /用户信息 -->
 
@@ -109,6 +116,9 @@
 
 <script>
 import { getArticleById } from '@/api/article'
+import { ImagePreview } from 'vant'
+import { addUserFollow, deleteUserFollow } from '@/api/user'
+
 export default {
   name: 'ArticleDetail',
   props: {
@@ -120,21 +130,27 @@ export default {
   },
   data () {
     return {
-      article: {},
-      loading: true,
-      errStatus: 0
+      article: {}, // 文章数据
+      loading: true, // 页面加载状态
+      errStatus: 0, // 错误状态
+      followLoading: false // 按钮点击状态
     }
   },
   created () {
     this.loadArticle()
   },
   methods: {
+    // 渲染文章详细信息
     async loadArticle () {
       this.loading = true
       try {
         const { data } = await getArticleById(this.articleId)
         this.article = data.data
-        console.log(this.article)
+        // 初始化图片预览,得先让数据渲染完成，延时器都是最后执行得
+        setTimeout(() => {
+          this.previewImage()
+        }, 0)
+        // console.log(this.article)
       } catch (error) {
         if (error.response && error.response.status === 404) {
           this.errStatus = 404
@@ -143,6 +159,46 @@ export default {
       }
       // 无论成功或者失败都关闭加载
       this.loading = false
+    },
+    // 图片预览
+    previewImage () {
+      // 得到所有的img节点
+      const articleContent = this.$refs['article-content']
+      const imgs = articleContent.querySelectorAll('img')
+      const imagesArr = [] // 定义一个图片地址数组
+      imgs.forEach((img, index) => {
+        imagesArr.push(img.src)
+        // 图片点击事件
+        img.onclick = () => {
+          // vant图片预览组件
+          ImagePreview({
+            images: imagesArr, // 循环图片数组
+            startPosition: index // 图片预览起始位置
+          })
+        }
+      })
+    },
+    // 关注用户
+    async onFollow () {
+      // 按钮点击加载状态
+      this.followLoading = true
+      try {
+        // 判断是否已经关注过了，执行相应的方法
+        if (this.article.is_followed) {
+          await deleteUserFollow(this.article.aut_id)
+        } else {
+          await addUserFollow(this.article.aut_id)
+        }
+        this.article.is_followed = !this.article.is_followed
+      } catch (error) {
+        let message = '操作失败请重试！'
+        if (error.response && error.response.status === 400) {
+          message = '你不能关注你自己！'
+        }
+        this.$toast(message)
+      }
+      // 点击后结束加载状态
+      this.followLoading = false
     }
   }
 }
@@ -168,7 +224,7 @@ export default {
       color: #3a3a3a;
     }
 
-    .user-info {
+    .art_user-info {
       padding: 0 32px;
       .avatar {
         width: 70px;
